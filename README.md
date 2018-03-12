18808578da5900127d6dcadb3f27ccf780aaaa16
Tomasz Michnik <tomasz_michnik@epam.com>
2018-03-12 15:50:08 +0100

    New notes and code

--- README.md
+++ README.md
@@ -1,2 +1,28 @@
 # Rest Assured, Serenity bdd framework
 
+#### Course files
+
+1. Run **rest.jar** from StudentApp
+
+   ```shell
+   java -jar rest.jar
+   # OR
+   java -jar rest.jar --server.port=8085
+   # for java 9
+   java -jar --add-modules java.xml.bind rest.jar
+   ```
+
+#### SERENITY BDD
+
+- open source test automation framework
+- integrated to work with many popular testing libraries like:JUNIT, SELENIUM2, APPIUM, JBEHAVE, CUCUMBER etc.
+- out of the box features for UI Automation (Web Driver Handling, Synchronization, Screenshots, Parallel testing, DataDriver testing, Cross browser testing, logging etc)
+- integrated with Rest-Assured to create API test automation without any hassle
+- integrates with popular build tools like MAVEN, GRADLE, ANT
+- reports
+- [project home page](http://www.thucydides.info/#/)
+- [user guide](http://www.thucydides.info/docs/serenity/)
+- [serenity on github](https://github.com/serenity-bdd/)
+- [serenity properties](http://www.thucydides.info/docs/serenity/#_serenity_system_properties_and_configuration)
+- [modeling requirements in junit](http://www.thucydides.info/docs/serenity/#_modelling_requirements_in_junit)
+
---
+++ StudentApp/src/test/java/com/studentapp/junit/studentsIdInfo/SerenityIdInfo.java
@@ -0,0 +1,72 @@
+package com.studentapp.junit.studentsIdInfo;
+
+import io.restassured.RestAssured;
+import net.serenitybdd.junit.runners.SerenityRunner;
+import net.serenitybdd.rest.SerenityRest;
+import net.thucydides.core.annotations.Manual;
+import net.thucydides.core.annotations.Pending;
+import org.junit.BeforeClass;
+import org.junit.Ignore;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+
+@RunWith(SerenityRunner.class)
+public class SerenityIdInfo {
+
+    @BeforeClass
+    public static void init() {
+
+        RestAssured.baseURI = "http://localhost:8080/student";
+
+    }
+
+    @Test
+    public void getAllStudents() {
+        SerenityRest.given()
+            .when()
+            .get("/list")
+            .then()
+            .log()
+            .all()
+            .statusCode(200);
+    }
+
+    @Test
+    public void thisIsaFailing() {
+        SerenityRest.given()
+            .when()
+            .get("/list")
+            .then()
+            .log()
+            .all()
+            .statusCode(500);
+    }
+    @Pending
+    @Test
+    public void thisIsaPendingTest() {
+    }
+    @Ignore
+    @Test
+    public void thisIsaSkippedTest() {
+    }
+
+    @Test
+    public void thisIsaTestWithError() {
+        System.out.println("This is an error"+(5/0));
+    }
+
+    @Test
+    public void fileDoesNotExist() throws FileNotFoundException {
+        File file = new File("E://file.txt");
+        FileReader fr = new FileReader(file);
+    }
+    @Manual
+    @Test
+    public void thisIsAManualTest()  {
+
+    }
+}
